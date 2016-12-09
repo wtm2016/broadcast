a74ace102acd5a04170cc343e7aebf0a555c5ff6
michwilk <michalwilk93@hotmail.com>
2016-12-09 19:32:22 +0100

    first

---
+++ app/src/main/java/com/example/m/myapplication/MyReceiver.java
@@ -0,0 +1,12 @@
+package com.example.m.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+public class MyReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+    }
+}
